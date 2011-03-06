330c5bec76a9de4576ec7b6827a29732e7284aed
fuensa82 <fuensa82@e32f7930-ee46-2b6a-d7d1-80ed37c43e7a>
2011-03-06 12:12:59 +0000

    Parte de contar los puntos

---
+++ F1Sopra/src/f1sopra/Conectar.java
@@ -0,0 +1,69 @@
+package f1sopra;
+
+import java.sql.*;
+import javax.naming.*;
+
+
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+
+public class Conectar {
+
+    private static final String driver = "com.mysql.jdbc.Driver";// = "COM.cloudscape.core.JDBCDriver";
+    private static final String driver2 = "org.gjt.mm.mysql.Driver";
+    //private static final String protocol1 = "jdbc:mysql://localhost/pruebasgecofuensa";
+    private static final String protocol1 = "jdbc:mysql://localhost/f1Sopra";
+    //private static final String protocol2 = "jdbc:mysql://localhost/";
+    
+    
+        
+    private static final String usuario="f1Sopra";
+    private static final String password = "Traicion";
+
+    private Conectar() {
+
+    }
+    /**
+     * Devuelve una conexion a la base de datos, para poder hacer consultas, ...
+     * @return Un objeto Connection para conectar con la base de datos ya definida en la clase.
+     * @throws javax.naming.NamingException
+     * @throws java.sql.SQLException
+     */
+    public static Connection getConnection() throws NamingException, SQLException {
+        /*if (driver == null || protocol1 == null) {
+            throw new SQLException("Driver not specified.");
+        }*/
+        /*System.out.println("Nueva conexion");
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUser("root");
+        dataSource.setPassword("03885536");
+        dataSource.setDatabaseName("GeCoFuensa");
+        dataSource.setServerName("localhost");
+
+        Connection conexion = dataSource.getConnection();
+        return conexion;
+*/
+
+        
+        //System.out.println("Conector");
+        try {
+            //System.out.println("Se procede a conectar");
+            Class.forName(driver).newInstance();
+            Connection connection = DriverManager.getConnection(protocol1, usuario, password);
+            //System.out.println("Se ha conectado");
+            return connection;
+        } catch (InstantiationException ex) {
+            System.out.println("error 1");
+        } catch (IllegalAccessException ex) {
+            System.out.println("error 2");
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Excepcion Driver");
+            System.out.println("Error excepcion: "+cnfe.getMessage());
+            cnfe.printStackTrace();
+            throw new SQLException("Driver <" + driver + "> not found in the classpath.");
+        }
+        return null;
+
+    }
+}
---
+++ F1Sopra/src/f1sopra/Puntos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package f1sopra;
+
+/**
+ *
+ * @author victor
+ */
+public interface Puntos {
+    static int POLE = 5;
+    static int PRIMERO = 25;
+    static int SEGUNDO = 18;
+    static int TERCERO = 15;
+    static int CUARTO = 12;
+    static int QUITO = 10;
+    static int SEXTO = 8;
+    static int SEPTIMO = 6;
+    static int OCTAVO = 4;
+    static int NOVENO = 2;
+    static int DECIMO = 1;
+    static int DENTRO = 1;
+    static int PODIUM = 50;
+    static int TODOS = 100;
+}
